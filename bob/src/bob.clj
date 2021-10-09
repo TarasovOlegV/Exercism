d54c4589a81945894b5d62e0514921b20fd10e67
TarasovOlegV <pgsmall1@gmail.com>
2021-10-09 13:11:47 +0400

    change bob v2.0

--- bob/src/bob.clj
+++ bob/src/bob.clj
@@ -1,23 +1,15 @@
 (ns bob)
 (require '[clojure.string :as str])
 
-(defn get-last-char [s]
-  (subs s (- (count s) 1)))
-
-(defn string-empty? [s]
-  (not (some? (re-seq #"[A-Za-z0-9-!$%^&*()_+|~=`{}\[\]:;'<>?,.\/]" s))))
-
 (defn capitalize? [s]
   (and (some? (re-seq  #"[A-Za-z]" s))
        (= s (str/upper-case s))))
 
 (defn response-for [string]
   (let [s (str/trim string)]
-    (cond (string-empty? s) "Fine. Be that way!"
-          (and
-           (capitalize? s)
-           (= "?" (get-last-char s)))  "Calm down, I know what I'm doing!"
-          (= "?" (get-last-char s)) "Sure."
+    (cond (empty? s) "Fine. Be that way!"
+          (and (capitalize? s) (= \? (last s)))  "Calm down, I know what I'm doing!"
+          (= \? (last s)) "Sure."
           (capitalize? s) "Whoa, chill out!"
           :else "Whatever.")))
 
